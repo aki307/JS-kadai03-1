081a63f63dc827f2caf143fa58856020736e3dd6
Ubuntu <ubuntu@ip-172-31-28-133.ap-northeast-1.compute.internal>
2023-06-25 03:05:25 +0000

    修正しました

--- main.js
+++ main.js
@@ -1,26 +1,40 @@
 const taskSubmit = document.getElementById('submit-button');
-const taskTable = document.getElementById('taskTable');
-let displayTasks = '';
+const taskTable = document.getElementById('task-table');
+const taskName = document.getElementById('task-name');
+let displayTasksHTML = '';
+const taskItem = document.getElementById('task-item');
 let tasks = [];
 
 taskSubmit.addEventListener('click', function(event) {
   event.preventDefault();
-  const newTask = document.forms[0].elements[0].value;
-  tasks.push(newTask);
-  displayTasks = '';
-  displayTasks += `<tr class="task_list task_item${tasks.length - 1}" id="tasklists">
+  const newTask = taskName.value;
+  tasks.push({
+    id: Object.keys(tasks).length,
+    taskName: newTask,
+    taskStatus: 'working',
+  });
+  displayTasksHTML = '';
+  displayTasksHTML += `
         <th>${tasks.length - 1}</th>
-        <th>${tasks[tasks.length - 1]}</th>
+        <th>${newTask}</th>
         <th> 
           <button type="button" class="working-button${tasks.length - 1}">作業中</button>
           <button type="button" class="delete-execution${tasks.length - 1}">削除</button>
         </th>
-      </tr>
       `;
-  if (tasks.length == 1) {
-    taskTable.insertAdjacentHTML('beforeend', displayTasks);
+
+  const newRow = document.createElement("tr");
+  newRow.innerHTML = displayTasksHTML;
+
+  const taskTableBody = taskTable.querySelector("tbody");
+  if (!taskTableBody) {
+    const taskTableBody = document.createElement("tbody");
+    taskTableBody.appendChild(newRow);
+    taskTable.appendChild(taskTableBody);
   }
   else {
-    taskTable.insertAdjacentHTML('beforeend', displayTasks);
+    taskTableBody.appendChild(newRow);
   }
+
+
 });
