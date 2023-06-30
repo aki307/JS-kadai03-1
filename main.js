76859ccb6effd9c3b3c66357df165a429afb4238
Ubuntu <ubuntu@ip-172-31-28-133.ap-northeast-1.compute.internal>
2023-06-30 11:19:36 +0000

    削除機能導

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ const taskSubmit = document.getElementById('submit-button');
 const taskTable = document.getElementById('task-table');
 const taskName = document.getElementById('task-name');
 let displayTasksHTML = '';
-const taskItem = document.getElementById('task-item');
-const tBody = document.getElementById('table-body');
 const tasks = [];
+const tBody = document.getElementById('table-body');
+
 
 taskSubmit.addEventListener('click', function(event) {
   event.preventDefault();
@@ -20,7 +20,7 @@ taskSubmit.addEventListener('click', function(event) {
         <th>${newTask}</th>
         <th> 
           <button type="button" class="working-button${tasks.length}">作業中</button>
-          <button type="button" class="delete-execution${tasks.length}">削除</button>
+          <button type="button" class="delete-button delete-execution${tasks.length}">削除</button>
         </th>
       `;
 
@@ -35,5 +35,45 @@ taskSubmit.addEventListener('click', function(event) {
     tBody.appendChild(newRow);
   }
 
-
 });
+
+document.addEventListener('click', function(event) {
+  if (event.target.classList.contains('delete-button')) {
+    const rowToRemove = event.target.closest('tr');
+    if (rowToRemove) {
+      const thElement = rowToRemove.querySelector('th');
+      if (thElement) {
+        const taskIndex = parseInt(thElement.textContent);
+        console.log(taskIndex);
+        let targetIndex = tasks.findIndex(task => task.id === taskIndex);
+        if (targetIndex !== -1) {
+          tasks.splice(targetIndex, 1);
+        }
+        rowToRemove.remove();
+
+        updateTaskIDs(taskIndex);
+        displayTasks();
+      }
+    }
+  }
+});
+
+
+function updateTaskIDs(deletedIndex) {
+  const taskRows = taskTable.querySelectorAll('tbody tr');
+  taskRows.forEach((row, index) => {
+    const thElement = row.querySelector('th:first-child');
+    if (thElement) {
+      const taskId = index + 1;
+      thElement.textContent = taskId;
+      tasks[index].id = taskId;
+    }
+  });
+}
+
+function displayTasks() {
+  const displayTasksElement = document.getElementById('displayTasks');
+  if (displayTasksElement) {
+    displayTasksElement.textContent = JSON.stringify(tasks);
+  }
+}
