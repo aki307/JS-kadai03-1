60dce6a21d0f503cb1dd451734d930ca98ee987a
Ubuntu <ubuntu@ip-172-31-28-133.ap-northeast-1.compute.internal>
2023-07-05 11:05:03 +0000

    タスク進捗機能追加

--- main.js
+++ main.js
@@ -1,33 +1,67 @@
 const taskSubmit = document.getElementById('submit-button');
+let submitNumber = 0;
 const taskTable = document.getElementById('task-table');
 const taskName = document.getElementById('task-name');
 let displayTasksHTML = '';
-const tasks = [];
+let tasks = [];
 const tBody = document.getElementById('table-body');
 
-
 taskSubmit.addEventListener('click', function(event) {
+  submitNumber += 1;
   event.preventDefault();
   const newTask = taskName.value;
   tasks.push({
-    id: Object.keys(tasks).length,
+    id: submitNumber,
     taskName: newTask,
     taskStatus: 'working',
   });
   displayTasksHTML = '';
   displayTasksHTML += `
-        <th>${tasks.length}</th>
+        <th id=${submitNumber}>${tasks.length}</th>
         <th>${newTask}</th>
       `;
-
+console.log(displayTasksHTML);
   const newRow = document.createElement("tr");
+  newRow.id= submitNumber;
   newRow.innerHTML = displayTasksHTML;
   const statusButton = document.createElement("button");
   statusButton.type = "button";
-  statusButton.id = `working-button${tasks.length}`;
   statusButton.textContent = "作業中";
+  statusButton.id = `status-button${submitNumber}`;
   statusButton.addEventListener("click", () => {
-    // ここは記入予定
+    console.log('押したボタンのidは' + statusButton.id);
+    const stateChange = newRow;
+    if (stateChange) {
+      const changeElement = stateChange.querySelector('th');
+      if (changeElement) {
+        const taskIndex = parseInt(changeElement.id);
+        console.log('changeElementは' + changeElement.id);
+        // let targetIndex = tasks.findIndex(task => task.id === taskIndex);
+        const targetId = tasks.find((task) => {
+          return task.id == taskIndex;
+        });
+        console.log('targetIdは' + targetId.id);
+
+        if (targetId !== undefined) {
+          tasks = tasks.map(obj => {
+            if (obj.id === taskIndex) {
+              console.log('obj.idは' + obj.id);
+              console.log('taskIndexは' + taskIndex);
+              if (obj.taskStatus === 'working') {
+                return { ...obj, taskStatus: 'completed' };
+              }
+              else if (obj.taskStatus === 'completed') {
+                return { ...obj, taskStatus: 'working' };
+              }
+            }
+            return obj;
+          });
+          console.log('idは'+ statusButton.id);
+          stateChangeRow(taskIndex, newRow);
+        }
+      }
+    }
+
   });
 
   const deleteButton = document.createElement("button");
@@ -54,7 +88,6 @@ taskSubmit.addEventListener('click', function(event) {
   else {
     tBody.appendChild(newRow);
   }
-
 });
 
 function deleteTask() {
@@ -62,8 +95,7 @@ function deleteTask() {
   if (rowToRemove) {
     const thElement = rowToRemove.querySelector('th');
     if (thElement) {
-      const taskIndex = parseInt(thElement.textContent);
-      console.log(taskIndex);
+      const taskIndex = parseInt(thElement.id);
       let targetIndex = tasks.findIndex(task => task.id === taskIndex);
       if (targetIndex !== -1) {
         tasks.splice(targetIndex, 1);
@@ -76,6 +108,9 @@ function deleteTask() {
   }
 }
 
+
+
+
 function updateTaskIDs(deletedIndex) {
   const taskRows = taskTable.querySelectorAll('tbody tr');
   taskRows.forEach((row, index) => {
@@ -83,7 +118,7 @@ function updateTaskIDs(deletedIndex) {
     if (thElement) {
       const taskId = index + 1;
       thElement.textContent = taskId;
-      tasks[index].id = taskId;
+      // tasks[index].id = taskId;
     }
   });
 }
@@ -92,5 +127,22 @@ function displayTasks() {
   const displayTasksElement = document.getElementById('displayTasks');
   if (displayTasksElement) {
     displayTasksElement.textContent = JSON.stringify(tasks);
+  }
+}
+
+function stateChangeRow(taskIndex,newRow) {
+  const taskRow = taskTable.querySelector(`tr[id="${taskIndex}"]`);
+  if (taskRow) {
+    const changeRow = taskRow.querySelector('th:nth-child(3) button[id*="status-button"]');
+    if (changeRow) {
+      if (changeRow.textContent === '作業中') {
+        changeRow.textContent = '完了';
+      }
+      else if (changeRow.textContent === '完了') {
+        changeRow.textContent = '作業中';
+      }
+      console.log(`タスクID: ${taskIndex} の状態が変更されました。`);
+      console.log('タスク情報:', tasks.find(task => task.id === taskIndex));
+    }
   }
 }
